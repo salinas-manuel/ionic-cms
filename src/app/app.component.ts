8f99d09c2064db19806498700bf280940c4fecb7
salinas-manuel <salinas.manuel_1980@yahoo.com>
2018-02-01 13:55:37 -0600

    Added a user and user view page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { UsersPage } from '../pages/users/users';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -22,6 +24,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
+      { title: 'Users', component: UsersPage },
       { title: 'List', component: ListPage }
     ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { UsersPage } from '../pages/users/users';
+import { UserViewPage } from '../pages/user-view/user-view';
+import { UserEditPage } from '../pages/user-edit/user-edit';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { UserProvider } from '../providers/user/user';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    UsersPage,
+    ListPage,
+    UserViewPage,
+    UserEditPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    UsersPage,
+    ListPage,
+    UserViewPage,
+    UserEditPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider
   ]
 })
 export class AppModule {}
